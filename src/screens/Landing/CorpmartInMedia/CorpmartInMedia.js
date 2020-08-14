a45ff5c8046c989377ada5e17985636b9b6ad917
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-08-14 23:44:38 +0530

    Added CorpMart in Media (Landing)

--- src/screens/Landing/CorpmartInMedia/CorpmartInMedia.js
+++ src/screens/Landing/CorpmartInMedia/CorpmartInMedia.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import styles from './CorpmartInMedia.module.css';
 
+import isoCert from './images/isocert.png';
+import startupIndia from './images/startupindia.png';
+
 const CorpmartInMedia = () => {
     return (
         <div className={styles.CorpmartInMedia}>
             <div className={styles.header}>
-                <div className={styles.title}>CorpMart in Media</div>
+                <div className={styles.title}>Accreditations</div>
                 <div className={styles.subtitle}>CorpMart in Media</div>
             </div>
             <div className={styles.mediaShowcase}>
-
+                <div>
+                    <img src={isoCert} />
+                </div>
+                <div>
+                    <img src={startupIndia} />
+                </div>
             </div>
         </div>
     )
