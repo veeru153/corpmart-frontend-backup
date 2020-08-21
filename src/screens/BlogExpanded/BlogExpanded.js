ae64abfb59078a15c5997861ec3cf7fe32243979
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-08-21 23:23:58 +0530

    Other Businesses in BusinessDetails fixed.

--- src/screens/BlogExpanded/BlogExpanded.js
+++ src/screens/BlogExpanded/BlogExpanded.js
@@ -1,34 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import styles from './BlogExpanded.module.css';
 import Navbar from '../../components/Navbar/Navbar';
 import NavbarMobile from '../../components/Navbar/NavbarMobile';
 import Footer from '../Landing/Footer/Footer';
+import Axios from '../../axios';
+import moment from 'moment';
 
-const BlogExpanded = (props) => {
-    if (!props.location.pathname.match(/\/blog\/.*/)) props.history.push('/blogs/');
-    useEffect(() => window.scrollTo(0,0), []);
+class BlogExpanded extends Component {
+    state = {
+        id: '',
+        title: '',
+        date: '',
+        author: '',
+        blogText: '',
+        imgUrl: '',
+    }
 
-    const { id, title, date, author, blogText, imgUrl } = props.location.state;
-    return (
-        <div>
-            <Navbar />
-            <NavbarMobile />
-            <div className={styles.BlogExpanded}>
-                <div className={styles.header}>
-                    <p className={styles.title}>{title}</p>
-                    <div className={styles.meta}>
-                        <p className={styles.author}>By - {author}</p>
-                        <p className={styles.date}>{date}</p>
+    async componentDidMount() {
+        if (!this.props.location.pathname.match(/\/blog\/.*/)) this.props.history.push('/blogs/');
+        window.scrollTo(0,0);
+        if(this.props.location.state != undefined) {
+            this.setState({
+                id: this.props.location.state.id,
+                title: this.props.location.state.title,
+                date: this.props.location.state.date,
+                author: this.props.location.state.author,
+                blogText: this.props.location.state.blogText,
+                imgUrl: this.props.location.state.imgUrl,
+            })
+        } else {
+            let id = window.location.pathname.split('/')[2];
+            try {
+                let res = await Axios.get(`/blog/${id}`);
+                let data = res.data;
+                this.setState({
+                    id: data.id,
+                    title: data.blog_title,
+                    date: moment(data.created_at).format("MMM DD, Y"),
+                    author: data.posted_by,
+                    blogText: data.blog_text,
+                    imgUrl: data.picture,
+                })
+            } catch (e) { console.log(e.response); }
+        }
+    }
+
+    // const { id, title, date, author, blogText, imgUrl } = props.location.state;
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <NavbarMobile />
+                <div className={styles.BlogExpanded}>
+                    <div className={styles.header}>
+                        <p className={styles.title}>{this.state.title}</p>
+                        <div className={styles.meta}>
+                            <p className={styles.author}>By - {this.state.author}</p>
+                            <p className={styles.date}>{this.state.date}</p>
+                        </div>
                     </div>
+                    {!this.state.imgUrl || this.state.imgUrl.length == 0
+                    ? null
+                    : <div className={styles.image}><img src={this.state.imgUrl} alt="Blog Image" /></div>}
+                    <div className={styles.text}>{this.state.blogText}</div>
                 </div>
-                {!imgUrl || imgUrl.length == 0
-                ? null
-                : <div className={styles.image}><img src={imgUrl} alt="Blog Image" /></div>}
-                <div className={styles.text}>{blogText}</div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    )
+        )
+    }
 }
 
 export default BlogExpanded;
--- src/screens/BusinessDetails/BusinessDetails.js
+++ src/screens/BusinessDetails/BusinessDetails.js
@@ -9,40 +9,60 @@ import Axios from '../../axios';
 import Cookies from 'universal-cookie';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
+import Carousel from 'react-elastic-carousel';
+import { withRouter } from 'react-router-dom';
 
-// TODO: Make the Other Business Card work.
 
 class BusinessDetails extends Component {
     state = {
-        loggedIng: false,
-        id: '',
-        desc: '',
-        type: '',
-        subtype: '',
-        industry: '',
-        yearOfIncorporation: '',
-        state: '',
-        authCapital: 0,
-        paidupCapital: 0,
-        askingPrice: 0,
-        gst: false,
-        bankAcc: false,
-        ieCode: false,
-        otherLicenses: false,
-        balancesheet: false,
-        balancesheetId: null,
-        hasContacted: false,
-        other: {},
+        currBusiness: {
+            id: '',
+            desc: '',
+            type: '',
+            subtype: '',
+            industry: '',
+            yearOfIncorporation: '',
+            state: '',
+            authCapital: 0,
+            paidupCapital: 0,
+            askingPrice: 0,
+            gst: false,
+            bankAcc: false,
+            ieCode: false,
+            otherLicenses: false,
+            balancesheet: false,
+            balancesheetId: null,
+            hasContacted: false,
+        },
+        other: [],
         error: false,
         errorMsg: '',
+        itemsToShow: 1,
+        carouselWidth: '100%'
     }
 
     async componentDidMount() {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
         const cookies = new Cookies();
         const token = cookies.get('userToken');
         const { match: { params } } = this.props;
         const { id } = params;
+        this.fetchBusinessDetails(token, id);
+        this.fetchOtherBusinesses();
+        if (window.innerWidth >= 1024) {
+            this.setState({
+                itemsToShow: 2,
+                carouselWidth: '60%',
+            })
+        } else if (window.innerWidth >= 1360) {
+            this.setState({
+                itemsToShow: 2,
+                carouselWidth: '50%',
+            })
+        }
+    }
+
+    fetchBusinessDetails = async (token, id) => {
         try {
             let res;
             if (token) {
@@ -57,25 +77,36 @@ class BusinessDetails extends Component {
             let data = await res.data[0];
             this.setState((prevState) => ({
                 ...prevState,
-                loggedIn: token ? true : false,
-                id: id,
-                desc: data.sale_description,
-                type: data.company_type,
-                subtype: data.sub_type,
-                industry: data.industry,
-                yearOfIncorporation: data.year_of_incorporation,
-                state: data.state,
-                authCapital: data.authorised_capital,
-                paidupCapital: data.paidup_capital,
-                gst: data.has_gst_number,
-                bankAcc: data.has_bank_account,
-                ieCode: data.has_import_export_code,
-                otherLicenses: data.has_other_license,
-                balancesheet: data.balancesheet_available,
-                balancesheetId: data.balancesheet_id,
-                hasContacted: data.has_contacted,
-                contactedNow: false,
+                currBusiness: {
+                    id: id,
+                    desc: data.sale_description,
+                    type: data.company_type,
+                    subtype: data.sub_type,
+                    industry: data.industry,
+                    yearOfIncorporation: data.year_of_incorporation,
+                    state: data.state,
+                    authCapital: data.authorised_capital,
+                    paidupCapital: data.paidup_capital,
+                    gst: data.has_gst_number,
+                    bankAcc: data.has_bank_account,
+                    ieCode: data.has_import_export_code,
+                    otherLicenses: data.has_other_license,
+                    balancesheet: data.balancesheet_available,
+                    balancesheetId: data.balancesheet_id,
+                    hasContacted: data.has_contacted,
+                    contactedNow: false,
+                },
             }))
+        } catch (e) { console.log(e.response); }
+    }
+
+    fetchOtherBusinesses = async () => {
+        try {
+            let res = await Axios.get(`/business-list/?format=json&page=1`);
+            let data = await res.data;
+            this.setState({
+                other: data.results
+            });
         } catch (e) { console.log(e.response); }
     }
 
@@ -99,7 +130,7 @@ class BusinessDetails extends Component {
         const token = cookies.get('userToken');
         const id = this.state.id;
         try {
-            if(token) {
+            if (token) {
                 let res = await Axios.post(`contact-request`, {
                     "business": id
                 }, {
@@ -114,9 +145,9 @@ class BusinessDetails extends Component {
             } else {
                 this.props.history.push('/login');
             }
-        } catch (e) { 
-            console.log(e.response); 
-            if(e.response.data["non_field_errors"][0].includes('unique field')) {
+        } catch (e) {
+            console.log(e.response);
+            if (e.response.data["non_field_errors"][0].includes('unique field')) {
                 this.setState({
                     error: true,
                     errorMsg: "Already Contacted"
@@ -133,38 +164,38 @@ class BusinessDetails extends Component {
                 <div className={styles.BusinessDetails}>
                     <div className={styles.selectedBusiness}>
                         <div className={styles.detailsCard}>
-                            <p className={styles.businessDesc}>{this.state.desc}</p>
+                            <p className={styles.businessDesc}>{this.state.currBusiness.desc}</p>
                             <div className={styles.businessInfo}>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Type</div>
-                                    <div className={styles.businessInfoValue}>{this.state.type}</div>
+                                    <div className={styles.businessInfoValue}>{this.state.currBusiness.type}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Sub Type</div>
-                                    <div className={styles.businessInfoValue}>{this.state.subtype}</div>
+                                    <div className={styles.businessInfoValue}>{this.state.currBusiness.subtype}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Industry</div>
-                                    <div className={styles.businessInfoValue} style={{ textTransform: 'capitalize' }}>{this.state.industry ? this.state.industry.toLowerCase() : "Available after contact"}</div>
+                                    <div className={styles.businessInfoValue} style={{ textTransform: 'capitalize' }}>{this.state.currBusiness.industry ? this.state.currBusiness.industry.toLowerCase() : "Available after contact"}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>State</div>
-                                    <div className={styles.businessInfoValue}>{this.state.state}</div>
+                                    <div className={styles.businessInfoValue}>{this.state.currBusiness.state}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Authorised Capital</div>
-                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal' }}>{this.state.authCapital ? `₹${this.state.authCapital}` : "Available After Contact"}</div>
+                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal' }}>{this.state.currBusiness.authCapital ? `₹${this.state.currBusiness.authCapital}` : "Available After Contact"}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Paid-Up Capital</div>
-                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal' }}>{this.state.paidCapital ? `₹${this.state.paidCapital}` : "Available After Contact"}</div>
+                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal' }}>{this.state.currBusiness.paidCapital ? `₹${this.state.currBusiness.paidCapital}` : "Available After Contact"}</div>
                                 </div>
                                 <div className={styles.businessInfoRow}>
                                     <div className={styles.businessInfoLabel}>Asking Price</div>
-                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal', fontSize: 16 }}>{this.state.askingPrice ? `₹${this.state.askingPrice}` : "Available After Contact"}</div>
+                                    <div className={styles.businessInfoValue} style={{ fontFamily: 'Helvetica Neue', fontWeight: 'normal', fontSize: 16 }}>{this.state.currBusiness.askingPrice ? `₹${this.state.currBusiness.askingPrice}` : "Available After Contact"}</div>
                                 </div>
                                 <div className={styles.businessRegRow}>
-                                    {this.state.gst
+                                    {this.state.currBusiness.gst
                                         ? <>
                                             <CheckCircleIcon style={{ fontSize: 16, color: '#55B546' }} />
                                             <p className={styles.regAvailable}>GST no. available</p>
@@ -176,7 +207,7 @@ class BusinessDetails extends Component {
                                     }
                                 </div>
                                 <div className={styles.businessRegRow}>
-                                    {this.state.bankAcc
+                                    {this.state.currBusiness.bankAcc
                                         ? <>
                                             <CheckCircleIcon style={{ fontSize: 16, color: '#55B546' }} />
                                             <p className={styles.regAvailable}>Bank account available</p>
@@ -188,7 +219,7 @@ class BusinessDetails extends Component {
                                     }
                                 </div>
                                 <div className={styles.businessRegRow}>
-                                    {this.state.ieCode
+                                    {this.state.currBusiness.ieCode
                                         ? <>
                                             <CheckCircleIcon style={{ fontSize: 16, color: '#55B546' }} />
                                             <p className={styles.regAvailable}>Import/Export code available</p>
@@ -200,7 +231,7 @@ class BusinessDetails extends Component {
                                     }
                                 </div>
                                 <div className={styles.businessRegRow}>
-                                    {this.state.otherLicenses
+                                    {this.state.currBusiness.otherLicenses
                                         ? <>
                                             <CheckCircleIcon style={{ fontSize: 16, color: '#55B546' }} />
                                             <p className={styles.regAvailable}>Other licenses available</p>
@@ -212,7 +243,7 @@ class BusinessDetails extends Component {
                                     }
                                 </div>
                                 <div className={styles.businessRegRow}>
-                                    {this.state.balancesheet
+                                    {this.state.currBusiness.balancesheet
                                         ? <>
                                             <CheckCircleIcon style={{ fontSize: 16, color: '#55B546' }} />
                                             <p className={styles.regAvailable}>Balancesheet available</p>
@@ -227,29 +258,29 @@ class BusinessDetails extends Component {
                         </div>
                         <div className={styles.balancesheet}>
                             <div className={styles.bsDiv1}>
-                                {this.state.balancesheet
+                                {this.state.currBusiness.balancesheet
                                     ? <p>Balancesheet of this business is available</p>
                                     : <p>Balancesheet of this business is not available</p>
                                 }
                                 <div className={styles.bsImg}></div>
                                 <Button
                                     label="View"
-                                    type={this.state.balancesheet ? "orange" : "#DADEE4"}
-                                    color={this.state.balancesheet ? "white" : "#676767"}
+                                    type={this.state.currBusiness.balancesheet ? "orange" : "#DADEE4"}
+                                    color={this.state.currBusiness.balancesheet ? "white" : "#676767"}
                                     style={{ padding: '12px 16px' }}
                                     textStyle={{ margin: 0 }}
                                     pressed={this.fetchBalancesheet}
-                                    disabled={!this.state.balancesheet}
+                                    disabled={!this.state.currBusiness.balancesheet}
                                 />
                             </div>
                             <div className={styles.bsDiv2}>
                                 <p>Interseted in acquiring this business?</p>
-                                {this.state.hasContacted
-                                ?   <p style={{ color: 'red'}}>We have already received your Contact Request.</p>
-                                :   null}
-                                {this.state.contactedNow && !this.state.hasContacted
-                                ?   <p style={{ color: '#4AB9CA'}}>Our executives will get back to you soon.</p>
-                                :   null}
+                                {this.state.currBusiness.hasContacted
+                                    ? <p style={{ color: 'red' }}>We have already received your Contact Request.</p>
+                                    : null}
+                                {this.state.currBusiness.contactedNow && !this.state.currBusiness.hasContacted
+                                    ? <p style={{ color: '#4AB9CA' }}>Our executives will get back to you soon.</p>
+                                    : null}
                                 <Button
                                     label="contact"
                                     type="orange"
@@ -264,19 +295,41 @@ class BusinessDetails extends Component {
                         <div className={styles.header}>
                             <p className={styles.title}>Other Businesses for sale</p>
                         </div>
-                        <BusinessSlide
-                            key={this.state.id}
-                            id={this.state.id}
-                            desc={this.state.sale_description}
-                            type={this.state.company_type}
-                            subtype={this.state.sub_type}
-                            industry={this.state.industry}
-                            state={this.state.state}
-                            authCapital={this.state.authorised_capital ?? 0}
-                            paidCapital={this.state.paidup_capital ?? 0}
-                            askingPrice={this.state.askingPrice ?? 0}
-                            className={styles.card}
-                        />
+                        <Carousel
+                            className={styles.carousel}
+                            style={{ width: this.state.carouselWidth }}
+                            itemsToShow={this.state.itemsToShow}
+                            enableAutoPlay
+                            renderPagination={() => <div></div>}
+                            showArrows={false}
+                        >
+                            {
+                                this.state.other.map(b => (
+                                    <div style={{ padding: '10px 0', display: 'flex', justifyContent: 'center', alignItems: 'items', width: '100%' }}>
+                                        <BusinessSlide
+                                            key={b.id}
+                                            id={b.id}
+                                            desc={b.sale_description}
+                                            type={b.company_type}
+                                            subtype={b.sub_type}
+                                            subtypeOther={b.sub_type_others_description}
+                                            industry={b.industry}
+                                            industryOther={b.industries_others_description}
+                                            state={b.state}
+                                            authCapital={b.authorised_capital ?? 0}
+                                            paidCapital={b.paidup_capital ?? 0}
+                                            askingPrice={b.admin_defined_selling_price ?? 0}
+                                            className={styles.card}
+                                            openBusiness={() => {
+                                                this.props.history.push(`/business-details/${b.id}`);
+                                                window.scrollTo(0, 0);
+                                                window.location.reload();
+                                            }}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </Carousel>
                     </div>
                 </div>
                 <Footer />
@@ -285,4 +338,4 @@ class BusinessDetails extends Component {
     }
 }
 
-export default BusinessDetails;
+export default withRouter(BusinessDetails);
